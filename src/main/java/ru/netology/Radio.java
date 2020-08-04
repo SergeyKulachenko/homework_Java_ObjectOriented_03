828bf129093c1f0390b04f8afa7a9bc7c12e6fcf
Sergey K <89184083452.mail.ru>
2020-08-04 17:15:12 +0300

    разъединение метода NumberStation на Next and Prev, and VoiumeStation на Up and Doun

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -5,17 +5,10 @@ import java.security.PrivateKey;
 public class Radio {
     private int currentNumberStation;
     private int enteredNumberStation;
-    private Boolean forwardOrBackward;
     private int currentSoundVolume;
-    private Boolean upOrDoun;
-
 
     public void setEnteredNumberStation(int enteredNumberStation) {
         this.enteredNumberStation = enteredNumberStation;
-    }
-
-    public void setForwardOrBackward(Boolean forwardOrBackward) {
-        this.forwardOrBackward = forwardOrBackward;
     }
 
     public void setCurrentNumberStation(int currentNumberStation) {
@@ -26,10 +19,6 @@ public class Radio {
         return currentNumberStation;
     }
 
-    public void setUpOrDoun(Boolean upOrDoun) {
-        this.upOrDoun = upOrDoun;
-    }
-
     public void setCurrentSoundVolume(int currentSoundVolume) {
         this.currentSoundVolume = currentSoundVolume;
     }
@@ -38,42 +27,43 @@ public class Radio {
         return currentSoundVolume;
     }
 
-    public void NumberStation() {
+    public void NumberStationNext() {
         if (enteredNumberStation != 10) {
             currentNumberStation = enteredNumberStation;
             this.currentNumberStation = currentNumberStation;
             return;
         }
-        if (forwardOrBackward == true) {
-            if (currentNumberStation == 9) currentNumberStation = -1;
-            currentNumberStation++;
-            this.currentNumberStation = currentNumberStation;
-            return;
-        } else {
-            if (currentNumberStation == 0) currentNumberStation = 10;
-            currentNumberStation--;
-            this.currentNumberStation = currentNumberStation;
-            return;
-        }
+        if (currentNumberStation == 9) currentNumberStation = -1;
+        currentNumberStation++;
+        this.currentNumberStation = currentNumberStation;
+        return;
     }
 
-    public void VolumeStation() {
-        if (upOrDoun == true) {
-            if (currentSoundVolume >= 10) {
-                currentSoundVolume = 10;
-                return;
-            } else {
-                currentSoundVolume++;
-                return;
-            }
+    public void NumberStationPrev() {
+        if (currentNumberStation == 0) currentNumberStation = 10;
+        currentNumberStation--;
+        this.currentNumberStation = currentNumberStation;
+        return;
+    }
+
+    public void VolumeStationUp() {
+        if (currentSoundVolume >= 10) {
+            currentSoundVolume = 10;
+            return;
         } else {
-            if (currentSoundVolume <= 0) {
-                currentSoundVolume = 0;
-                return;
-            } else {
-                currentSoundVolume--;
-                return;
-            }
+            currentSoundVolume++;
+            return;
+        }
+
+    }
+
+    public void VolumeStationDoun() {
+        if (currentSoundVolume <= 0) {
+            currentSoundVolume = 0;
+            return;
+        } else {
+            currentSoundVolume--;
+            return;
         }
     }
 }
--- src/test/java/ru/netology/RadioTest.java
+++ src/test/java/ru/netology/RadioTest.java
@@ -8,41 +8,61 @@ import static org.junit.jupiter.api.Assertions.*;
 class RadioTest {
     @ParameterizedTest
     @CsvSource({
-            "NumberStationEnter,2,,6,6",
-            "NumberStationFrom_0Forward,0,true,10,1",
-            "NumberStationFrom_5Forward,5,true,10,6",
-            "NumberStationFrom_9Forward,9,true,10,0",
-            "NumberStationFrom_0Forward,0,false,10,9",
-            "NumberStationFrom_5Forward,5,false,10,4",
-            "NumberStationFrom_9Forward,9,false,10,8"
+            "NumberStationEnter,2,6,6",
+            "NumberStationFrom_0Forward,0,10,1",
+            "NumberStationFrom_5Forward,5,10,6",
+            "NumberStationFrom_9Forward,9,10,0",
     })
-    public void NumberStation(String name, int currentNumberStation, Boolean forwardOrBackward,
-                              int enteredNumberStation, int expected) {
+    public void NumberStationNext(String name, int currentNumberStation,
+                                  int enteredNumberStation, int expected) {
         Radio station = new Radio();
         station.setCurrentNumberStation(currentNumberStation);
-        station.setForwardOrBackward(forwardOrBackward);
         station.setEnteredNumberStation(enteredNumberStation);
-        station.NumberStation();
+        station.NumberStationNext();
         int actual = station.getCurrentNumberStation();
         assertEquals(expected, actual);
     }
 
     @ParameterizedTest
     @CsvSource({
-            "volumeUpFrom_0,0,true,1",
-            "volumeUpFrom_5,5,true,6",
-            "volumeUpFrom_max,10,true,10",
-            "volumeDownFrom_10,10,False,9",
-            "volumeDownFrom_6,6,False,5",
-            "volumeDownFrom_min,0,False,0"
+            "NumberStationFrom_0Forward,0,9",
+            "NumberStationFrom_5Forward,5,4",
+            "NumberStationFrom_9Forward,9,8"
     })
-    public void VolumeStation(String name, int currentSoundVolume, Boolean upOrDoun, int expected) {
+    public void NumberStationPrev(String name, int currentNumberStation, int expected) {
+        Radio station = new Radio();
+        station.setCurrentNumberStation(currentNumberStation);
+        station.NumberStationPrev();
+        int actual = station.getCurrentNumberStation();
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "volumeUpFrom_0,0,1",
+            "volumeUpFrom_5,5,6",
+            "volumeUpFrom_max,10,10"
+
+    })
+    public void VolumeStationUp(String name, int currentSoundVolume, int expected) {
         Radio station = new Radio();
         station.setCurrentSoundVolume(currentSoundVolume);
-        station.setUpOrDoun(upOrDoun);
-        station.VolumeStation();
+        station.VolumeStationUp();
         int actual = station.getCurrentSoundVolume();
         assertEquals(expected, actual);
     }
 
+    @ParameterizedTest
+    @CsvSource({
+            "volumeDownFrom_10,10,9",
+            "volumeDownFrom_6,6,5",
+            "volumeDownFrom_min,0,0"
+    })
+    public void VolumeStationDoun(String name, int currentSoundVolume, int expected) {
+        Radio station = new Radio();
+        station.setCurrentSoundVolume(currentSoundVolume);
+        station.VolumeStationDoun();
+        int actual = station.getCurrentSoundVolume();
+        assertEquals(expected, actual);
+    }
 }
